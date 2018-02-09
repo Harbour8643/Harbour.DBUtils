ec5de6d4abb6fab41c2619b8d863222bf0460bf8
Harbour <864353016@qq.com>
2018-02-09 12:13:46 +0800

    简化程序

--- Harbour.DBUtils/Properties/AssemblyInfo.cs
+++ Harbour.DBUtils/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Maticsoft.DBUtility(20160107)")]
+[assembly: AssemblyTitle("Harbour.DBUtils")]
 [assembly: AssemblyDescription("Data Access Application Model By Harbour")]
 [assembly: AssemblyConfiguration("LiTianPing")]
-[assembly: AssemblyCompany("HRFD")]
-[assembly: AssemblyProduct("Maticsoft.DBUtility")]
-[assembly: AssemblyCopyright("Copyright (C) HRFD 2016-2017")]
+[assembly: AssemblyCompany("Harbour")]
+[assembly: AssemblyProduct("Harbour.DBUtils")]
+[assembly: AssemblyCopyright("Copyright (C) Harbour 2016-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("4.0.0")]
+[assembly: AssemblyVersion("5.0.0")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
